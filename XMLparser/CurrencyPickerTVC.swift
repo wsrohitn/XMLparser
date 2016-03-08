11d0ec406cf2ab475b92137ad8fbc5b491ce7d0c
Rohit Natarajan <rohitnatarajan@jonathans-mac-mini.local>
2016-03-08 15:05:41 +0000

    first commit

---
+++ XMLparser/CurrencyPickerTVC.swift
@@ -0,0 +1,112 @@
+//
+//  CurrencyPickerTVC.swift
+//  XMLparser
+//
+//  Created by Rohit Natarajan on 08/03/2016.
+//  Copyright © 2016 Rohit Natarajan. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyPickerTVC: UITableViewController {
+
+    class func loadVC(sb:UIStoryboard, nc:UINavigationController, parent: CurrencyPickerParent) {
+        if let vc = sb.instantiateViewControllerWithIdentifier("CurrencyPickerTVC") as? CurrencyPickerTVC {
+            vc.setInitialState(parent)
+            nc.pushViewController(vc, animated: true)
+        }        
+    }
+    
+    var parent: CurrencyPickerParent?
+    
+    func setInitialState(parent: CurrencyPickerParent) {
+        self.parent = parent
+        self.title = "Choose from \(parent.currencyList.count) currencies"
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return parent!.currencyList.count
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("CurrencyCell", forIndexPath: indexPath)
+
+        // Configure the cell...
+        cell.textLabel?.text = parent!.currencyList[indexPath.row]
+        return cell
+    }
+    
+    var selectedCurrency = ""
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        selectedCurrency = parent!.currencyList[indexPath.row]
+        parent!.setSelectedCurrency(selectedCurrency)
+        print(selectedCurrency)
+        self.navigationController?.popViewControllerAnimated(true)
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- XMLparser/ViewController.swift
+++ XMLparser/ViewController.swift
@@ -8,18 +8,138 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+protocol CurrencyPickerParent {
+    
+    var currencyList: [String]{
+        get
+    }
+    
+    func setSelectedCurrency(currency : String)
+}
+class ViewController: UIViewController, NSXMLParserDelegate, UITableViewDataSource, CurrencyPickerParent {
 
     override func viewDidLoad() {
+        print("hello world")
         super.viewDidLoad()
+        title = baseCurrency
+        tableView.dataSource = self
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Pick", style: .Plain, target: self, action: "pickCurrency")
+        
+        getRates()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func setSelectedCurrency(currency : String) {
+        baseCurrency = currency
+        getRates()
+    }
 
+
+    func pickCurrency(){
+        print("pick currency")
+        CurrencyPickerTVC.loadVC(self.storyboard!, nc: self.navigationController!, parent: self)
+        print("new currency selected")
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myItems.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("RateCell", forIndexPath: indexPath)
+        print(indexPath)
+        let item = myItems[indexPath.row]
+        cell.textLabel?.text = item["targetCurrency"]
+        cell.detailTextLabel?.text = item["exchangeRate"]
+        return cell
+    }
 
+    var xmlParser : NSXMLParser?
 
+    func getRates() {
+        let urlString = NSURL(string: "http://www.floatrates.com/daily/\(baseCurrency).xml")
+        let rssUrlRequest:NSURLRequest = NSURLRequest(URL:urlString!)
+        myItems = []
+        currencyList = []
+        tableView.reloadData()
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(rssUrlRequest) {
+            data, response, error in
+            self.xmlParser = NSXMLParser(data: data!)
+            self.xmlParser!.delegate = self
+            self.xmlParser!.parse()
+            print(self.myItems.count, "exchange rates")
+            dispatch_async(dispatch_get_main_queue()){
+                self.afterLoad()
+            }
+        }
+        
+        task.resume()
+    }
+    
+    func afterLoad() {
+        tableView.reloadData()
+        title = "\(baseCurrency) against \(currencyList.count) currencies"
+    }
+    
+    var currentKey = ""
+    var baseCurrency = "GBP"
+    var currencyList = [String]()
+    /*class exchangeRateItem {
+        var dict = [String:String]()
+    }*/
+    
+    var currentItem: [String:String]?
+    var myItems = [[String:String]]()
+    
+    func parser(parser: NSXMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String]) {
+        
+        currentKey = elementName
+        if currentKey == "item" {
+            currentItem = [String:String]()
+        }
+        
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+    func parser(parser: NSXMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        
+        if elementName == "item" {
+            if let item = currentItem {
+                myItems.append(item)
+                currencyList.append(item["targetCurrency"]!)
+                print(item["baseCurrency"]!, item["targetCurrency"]!, item["exchangeRate"]!, myItems.count)
+                currentItem = nil
+            }
+        }
+    }
+
+    func parser(parser: NSXMLParser, foundCharacters string: String) {
+        
+        let trimmed = string.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+        if currentItem != nil {
+            if let _ = currentItem![currentKey]{
+                currentItem![currentKey]! += trimmed
+            }
+            else {
+                currentItem![currentKey] = trimmed
+            }
+        }
+//        if var item = currentItem {
+//            if let _ = item[currentKey]{
+//                item[currentKey]! += trimmed
+//            }
+//            else {
+//                item[currentKey] = trimmed
+//            }
+//            print("keys", currentItem!.count, item.count)
+//        }
+    }
+    
+    
+   
 }
 
